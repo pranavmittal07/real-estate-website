598e3ead98cd7eb4a1c4bfc5e9d2d69ee3044408
Pranav Mittal <pranavmittal.in@gmail.com>
2024-09-05 19:19:12 +0530

    express backend + db connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter,Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignOut from "./pages/SignOut"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
-const Hello = () => {
+const App = () => {
   return (
-    <div>Hello</div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/sign-in" element={<SignIn />}/>
+      <Route path="/sign-out" element={<SignOut />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
-export default Hello
+export default App
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { FaSearch } from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className=' bg-slate-300 shadow-md'>
+        <div className='flex justify-between items-center p-3'>
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'> 
+                <span className='text-slate-500'>Apna</span>
+                <span className='text-slate-700'>Ghar</span>
+            </h1>
+            <form className='flex'>
+                <input type="text" placeholder='Search...' className='bg-transparent focus:outline-5'/>
+                <FaSearch className='text-slate-600'/>
+            </form>
+
+            <ul className='flex gap-4'>
+                <Link to="/">
+                <li className='text-slate-700 hidden sm:inline  hover:underline'>Home</li>
+                </Link>
+                <Link to="/about">
+                <li className='text-slate-700 hidden sm:inline  hover:underline'>About</li>
+                </Link>
+                <Link to="/sign-in">
+                <li className='text-slate-700   hover:underline'>Sign In</li>
+                </Link>
+            </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
